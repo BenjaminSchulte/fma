409abd1c5bdb395e7bf8f339632d1a8cf10ab5a7
Benjamin Schulte <benjamin@dokomi.de>
2017-11-12 20:42:52 +0100

    [BUGFIX] ignore empty blocks

--- src/linker/StaticCodeBlock.js
+++ src/linker/StaticCodeBlock.js
@@ -16,21 +16,43 @@ export default class StaticCodeBlock {
     return this.locationHint;
   }
 
+  isEmpty() {
+    return this.getSize() == 0;
+  }
+
   registerInMemory(memory) {
+    if (this.isEmpty()) {
+      return;
+    }
+
     const child = memory.createChild();
     child.setItemSize(this.getSize());
     child.setName(this.code.getFirstSymbol());
 
     this.locationHint.applyTo(child);
-
     this.location = child;
   }
 
   collectSymbols(symbols) {
-    const address = this.location.getMemoryAddress().getApplicationAddress();
+    if (this.isEmpty()) {
+      this.collectSymbolsForEmpty(symbols);
+      return;
+    }
 
+    if (!this.location.hasMemoryAddress()) {
+      throw new Error('Could not find memory address for symbols: ' + this.code.getSymbols().map(symbol => symbol.name).join(', '));
+      return;
+    }
+
+    const address = this.location.getMemoryAddress().getApplicationAddress();
     for (let symbol of this.code.getSymbols()) {
       symbols.add(symbol.name, symbol.offset + address);
+    }
+  }
+
+  collectSymbolsForEmpty(symbols) {
+    for (let symbol of this.code.getSymbols()) {
+      symbols.add(symbol.name, 0);
     }
   }
 
@@ -39,6 +61,10 @@ export default class StaticCodeBlock {
   }
 
   getRomOffset() {
+    if (this.isEmpty()) {
+      return 0;
+    }
+
     return this.location.getMemoryAddress().getRomOffset();
   }
 
